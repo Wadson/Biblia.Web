b5b70115433a7e9a03c2cdebc0d255ad4b556c3c
Wadson <wadsonwadson@hotmail.com>
2026-09-18 23:36:14 -0300

    Aplicado Css incluido ajuda

--- Biblia.Web.AppHost/obj/Release/net10.0/Biblia.Web.AppHost.AssemblyInfo.cs
+++ Biblia.Web.AppHost/obj/Release/net10.0/Biblia.Web.AppHost.AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblia.Web.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce56122621866c984a0c95e38f65dcd8f02627dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6489693b5c75710520a2a8415ac70e7790181b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblia.Web.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblia.Web.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Biblia.Web/obj/Release/net10.0/win-x64/Biblia.Web.AssemblyInfo.cs
+++ Biblia.Web/obj/Release/net10.0/win-x64/Biblia.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblia.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce56122621866c984a0c95e38f65dcd8f02627dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6489693b5c75710520a2a8415ac70e7790181b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblia.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblia.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.1.0.0")]
